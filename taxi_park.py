143322569da2379eae0afbcd3fc2ca92dd343fe1
OlegRMW <117780263+OlegRMW@users.noreply.github.com>
2025-09-04 12:55:01 +0300

    Create taxi_park.py

---
+++ taxi_park.py
@@ -0,0 +1,22 @@
+class TaxiPark:
+
+    def __init__(self):
+        self.drivers = [1]
+        self.cars = [] 
+
+    def __repr__(self):
+        print(f'Авто таксопарка: {self.cars}')
+
+    def add_car():
+        pass
+    def add_driver():
+        pass 
+    def print_info():
+        pass 
+    def fire_driver():
+        pass 
+
+
+taxi_park = TaxiPark()
+
+print(taxi_park)
