db9eb591225aee781ebca94cc7f35cb0c0b4fa4d
OlegRMW <zagudpev@mail.ru>
2025-09-05 20:16:02 +0300

    make structure

---
+++ practice_1/order.py
@@ -0,0 +1,2 @@
+class Oreder:
+    pass 
--- practice_1/taxi_park.py
+++ practice_1/taxi_park.py
@@ -1,22 +1,35 @@
+from car import Car 
+
 class TaxiPark:
 
     def __init__(self):
-        self.drivers = [1]
-        self.cars = [] 
+        self.__drivers = []
+        self.__cars = [] 
 
     def __repr__(self):
-        print(f'Авто таксопарка: {self.cars}')
+        # return f'Авто таксопарка: {self.__cars}'
+        return f'{self.__dict__}'
 
-    def add_car():
+    def add_car(car: Car):
         pass
+    
     def add_driver():
         pass 
+    
     def print_info():
         pass 
+    
     def fire_driver():
         pass 
-
+    
+    @property
+    def drivers(self):
+        return self.__drivers
+    
+    @property
+    def cars(self):
+        return self.__cars 
 
 taxi_park = TaxiPark()
 
-print(taxi_park)
+print(taxi_park.drivers)
